ec730ebe2f7bf2859ae471c747500b9594c24df3
Alejandro CR <contacto@alejandrocr.co>
2022-08-15 22:42:29 -0500

    harder immutability

--- value_object.py
+++ value_object.py
@@ -1,32 +1,57 @@
+"""
+Value object abstraction.
+"""
+
 from __future__ import annotations
 from typing import Any
 
 from abc import ABC, abstractmethod
 
+
 class ValueObject(ABC):
+    """
+    Represents a entity whose value is its identity.
+    """
     _value: Any
+    _initialized = False
+
+    @abstractmethod
+    def is_valid(self, value) -> bool:
+        """
+        Checks if the value is valid for this value object.
+        """
 
     @property
     def value(self) -> Any:
+        """
+        Returns the value of this value object.
+        """
         return self._value
 
-    @value.setter
-    def value(self, value: Any) -> None:
-        raise Exception('cannot set value of immutable value object')
-
     def __init__(self, value: Any):
+
         if not self.is_valid(value):
             raise ValueError('not valid value for this value object')
+
         self._value = value
+        self._initialized = True
+
+    def __setattr__(self, name, value) -> None:
+        if self._initialized:
+            raise AttributeError('Cannot assign attributes to this class')
+        else:
+            super().__setattr__(name, value)
+
+    def __delattr__(self, __name: str) -> None:
+        raise AttributeError('Cannot delete attributes from this class')
 
     def __eq__(self, other: ValueObject) -> bool:
         if not isinstance(other, self.__class__):
             return False
-        return self.value == other.value
+        return self._value == other._value
+
+    def __ne__(self, other: ValueObject) -> bool:
+        return not self.__eq__(other)
 
     def __str__(self) -> str:
-        return str(self.value)
-
-    @abstractmethod
-    def is_valid(self, value) -> bool:
-        pass
+        return str(self._value)
