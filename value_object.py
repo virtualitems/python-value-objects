6712c3ec56ed0b2efc29304c5fca82bf14939f38
Alejandro CR <contacto@alejandrocr.co>
2022-08-15 23:43:29 -0500

    refactor value object to namedtuple

--- value_object.py
+++ value_object.py
@@ -1,55 +1,38 @@
 """
-Value object abstraction.
+Module that provides a value object abstraction for model data objects.
+
+* ValueObject   Value object abstraction.
+* ImmutabeValue Named tuple with a single value.
 """
 
+# syntax
 from __future__ import annotations
 from typing import Any
 
+# abstractions
 from abc import ABC, abstractmethod
 
+# dependencies
+from collections import namedtuple
 
-class ValueObject(ABC):
+
+ImmutabeValue = namedtuple('ImmutabeValue', ('value',))
+
+
+class ValueObject(ABC, ImmutabeValue):
     """
     Represents a entity whose value is its identity.
     """
-    _value: Any
-    _initialized = False
-
     @abstractmethod
-    def is_valid(self, value) -> bool:
+    def is_valid(self, value: Any) -> bool:
         """
         Checks if the value is valid for this value object.
         """
 
-    @property
-    def value(self) -> Any:
-        """
-        Returns the value of this value object.
-        """
-        return self._value
-
     def __init__(self, value: Any):
-
         if not self.is_valid(value):
-            raise ValueError('not valid value for this value object')
-
-        self._value = value
-        self._initialized = True
-
-    def __setattr__(self, name, value) -> None:
-        assert not self._initialized, 'Cannot set attributes on this class'
-        super().__setattr__(name, value)
-
-    def __delattr__(self, __name: str) -> None:
-        assert False, 'Cannot delete attributes on this class'
-
-    def __eq__(self, other: ValueObject) -> bool:
-        if not isinstance(other, self.__class__):
-            return False
-        return self._value == other._value
-
-    def __ne__(self, other: ValueObject) -> bool:
-        return not self.__eq__(other)
+            raise ValueError(f'not valid value for {self.__class__} value object')
+        super().__init__()
 
     def __str__(self) -> str:
-        return str(self._value)
+        return str(self.value)
